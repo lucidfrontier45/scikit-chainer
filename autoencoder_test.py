9d91beebcd677cf3d0f2dcad5cc544ba2bf86d60
Shiqiao Du <lucidfrontier.45@gmail.com>
2015-07-29 00:17:55 +0900

    updated example code

--- autoencoder_test.py
+++ autoencoder_test.py
@@ -17,16 +17,16 @@ hidden_dim = 10
 
 x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
 
-model = LogisticRegression(optimizer=optimizers.SGD(), eps=1e-4,
+model = LogisticRegression(optimizer=optimizers.RMSprop(), eps=1e-5, report=1000,
                            n_dim=input_dim, n_classes=output_dim).fit(x_train, y_train)
 
 print("LogisticRegressoin Score")
 print(model.score(x_test, y_test))
 
 model = Pipeline([
-    ("AutoEncoder", DenoisingAutoEncoder(optimizer=optimizers.SGD(), eps=1e-4,
-                                input_dim=input_dim, hidden_dim=hidden_dim, report=100)),
-    ("LogisticRegression", LogisticRegression(optimizer=optimizers.SGD(), eps=1e-4,
+    ("AutoEncoder", DenoisingAutoEncoder(optimizer=optimizers.RMSprop(), eps=1e-5,
+                                         input_dim=input_dim, hidden_dim=hidden_dim, report=1000)),
+    ("LogisticRegression", LogisticRegression(optimizer=optimizers.RMSprop(), eps=1e-5, report=1000,
                                               n_dim=hidden_dim, n_classes=output_dim))
 ]).fit(x_train, y_train)
 

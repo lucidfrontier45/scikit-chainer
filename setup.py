c02494f9579dc54348679b056f7e4fd9bf231434
Shiqiao Du <s.du@freebit.net>
2015-07-28 10:31:44 +0900

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ classifiers = [
 
 setup(
     name='scikit-chainer',
-    version='0.1.1',
+    version='0.2',
     packages=['skchainer'],
     url='https://github.com/lucidfrontier45/scikit-chainer',
     license='MIT',
